d3742b0826e62898e36edd8d877f7d1a69e0090f
Moise1 <rwibutso.mo@gmail.com>
2022-09-26 20:45:03 +0200

    added navigation

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import {Home} from './src/screens/Home';
+import { registerRootComponent } from "expo";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Home } from "./src/screens/Home";
+import { Details } from "./src/screens/Details";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
- 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Home/>
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen name="Details" component={Details} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+registerRootComponent(App);
---
+++ src/screens/Details.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export const Details = () => {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({})
